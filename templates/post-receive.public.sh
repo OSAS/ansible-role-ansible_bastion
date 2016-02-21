2a7bff8b25d14ca9da4a2f2dae044d78ba0558b1
Michael Scherer <misc@redhat.com>
2016-02-21 16:25:17 +0100

    Remove '-c' option, not used with sudo

--- templates/post-receive.public.sh
+++ templates/post-receive.public.sh
@@ -28,5 +28,5 @@ do
         # update /etc/ansible
         sudo -n /usr/local/bin/update_ansible_config.sh
         # run ansible
-        sudo -n -u {{ ansible_username }} -c /usr/local/bin/generate_ansible_command.py {{ '--compat' if not compat_disable else '' }} --old $OLDREV --new $NEWREV --git $(pwd)
+        sudo -n -u {{ ansible_username }} /usr/local/bin/generate_ansible_command.py {{ '--compat' if not compat_disable else '' }} --old $OLDREV --new $NEWREV --git $(pwd)
 done

193e0c6b42e0854194c880c3c8212452314ee147
Michael Scherer <misc@redhat.com>
2016-04-25 10:39:14 +0200

    Add a safeguard to verify that update_galaxy can run

--- files/update_galaxy.sh
+++ files/update_galaxy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+[ -w /etc/ansible/roles ] || (echo "Cannot write /etc/ansible/roles, aborting"; exit 1)
 cd /etc/ansible
 /usr/bin/ansible-galaxy install -f -r /etc/ansible/requirements.yml

ea6877e1cfa54555136ce498beaf4c746d418290
Michael Scherer <misc@redhat.com>
2022-01-22 14:19:13 +0100

    Update galaxy to handle new requirements format

--- files/update_galaxy.sh
+++ files/update_galaxy.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
-[ -w /etc/ansible/roles ] || (echo "Cannot write /etc/ansible/roles, aborting"; exit 1)
+REQ_PATH=/etc/ansible/requirements.yml
+ROLES_PATH=/etc/ansible/roles
+INSTALL_ARGS="install -f -r $REQ_PATH"
+INSTALL_ROLES_ARGS="$INSTALL_ARGS -p $ROLES_PATH"
+
+[ -w $ROLES_PATH ] || (echo "Cannot write $ROLES_PATH, aborting"; exit 1)
 cd /etc/ansible
-/usr/bin/ansible-galaxy install -f -r /etc/ansible/requirements.yml -p /etc/ansible/roles >/dev/null
+NEW=0
+if grep -q 'roles:' $REQ_PATH; then
+	/usr/bin/ansible-galaxy role $INSTALL_ROLES_ARGS
+	NEW=1
+fi;
+
+if grep -q 'collections:' $REQ_PATH; then
+	/usr/bin/ansible-galaxy collection $INSTALL_ARGS
+	NEW=1
+fi;
+
+if [ $NEW -ne 1 ]; then
+	/usr/bin/ansible-galaxy $INSTALL_ROLES_ARGS >/dev/null
+fi;
